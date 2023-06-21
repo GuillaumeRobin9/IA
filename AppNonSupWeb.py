00b9fa2179335edc752f1661c44e384cad55861d
epeigne <peigne.enzo@gmail.com>
2023-06-21 09:33:58 +0200

    first prototype

---
+++ AppNonSupWeb.py
@@ -0,0 +1,27 @@
+import json
+from sklearn.cluster import KMeans
+import sys
+
+def k_means_accident(latitude, longitude, centroids):
+    # Création des données d'entrée
+    accident_data = [[latitude, longitude]]
+
+    # Initialisation du modèle k-means
+    kmeans = KMeans(n_clusters=len(centroids), init=centroids, n_init=1)
+    
+    # Entraînement du modèle sur les données d'entrée
+    kmeans.fit(accident_data)
+    
+    # Prédiction du cluster d'appartenance de l'accident
+    cluster = kmeans.predict(accident_data)[0]
+    
+    # Conversion du résultat en format JSON
+    result = {'cluster': cluster}
+    json_result = json.dumps(result)
+    
+    return json_result
+
+
+
+json_result = k_means_accident(float(sys.argv[1]), float(sys.argv[2]), json.loads(sys.argv[3]))
+print(json_result)
---
+++ AppSupWeb.py
@@ -0,0 +1,29 @@
+import json
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import sys
+
+def knn_accident(accident_info, csv_file):
+    # Chargement des données d'entraînement depuis le fichier CSV
+    accidents = pd.read_csv(csv_file)
+    X = accidents.drop('gravite', axis=1)
+    y = accidents['gravite']
+    
+    # Initialisation du modèle KNN
+    knn = KNeighborsClassifier()
+    
+    # Entraînement du modèle sur les données d'entraînement
+    knn.fit(X, y)
+    
+    # Prédiction de la gravité de l'accident
+    gravite = knn.predict([accident_info])[0]
+    
+    # Conversion du résultat en format JSON
+    result = {'gravite': gravite}
+    json_result = json.dumps(result)
+    
+    return json_result
+
+
+json_result = knn_accident(json.loads(sys.argv[1]), sys.argv[2])
+print(json_result)
