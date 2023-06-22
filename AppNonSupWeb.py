cd7aedce3f87ec5fb4ff9aff2a27a824711e027a
epeigne <peigne.enzo@gmail.com>
2023-06-22 11:46:38 +0200

    updates

--- AppNonSupWeb.py
+++ AppNonSupWeb.py
@@ -7,25 +7,26 @@ def k_means_accident(latitude, longitude, centroids):
     accident_data = [[latitude, longitude]]
 
     # Initialisation du modèle k-means
-    kmeans = KMeans(n_clusters=len(centroids), init=centroids, n_init=1)
+    cluster_results = []
+    for centroid in centroids:
+        kmeans = KMeans(n_clusters=1, init=[centroid], n_init=1)
     
-    # Entraînement du modèle sur les données d'entrée
-    kmeans.fit(accident_data)
+        # Entraînement du modèle sur les données d'entrée
+        kmeans.fit(accident_data)
     
-    # Prédiction du cluster d'appartenance de l'accident
-    cluster = kmeans.predict(accident_data)[0]
+        # Prédiction du cluster d'appartenance de l'accident
+        cluster = kmeans.predict(accident_data)[0]
+        cluster_results.append(int(cluster))
     
     # Conversion du résultat en format JSON
-    result = {'cluster': cluster}
-    json_result = json.dumps(result)
+    results = {'clusters': cluster_results}
+    json_result = json.dumps(results)
     
     return json_result
-
-
 
 json_result = k_means_accident(float(sys.argv[1]), float(sys.argv[2]), json.loads(sys.argv[3]))
 print(json_result)
 
-#export du resultat json_result dans un fichier json
+# Export du résultat json_result dans un fichier json
 with open('json/result_nonSup.json', 'w') as outfile:
-    json.dump(json_result, outfile)
+    outfile.write(json_result)
